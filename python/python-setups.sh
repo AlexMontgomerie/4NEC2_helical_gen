07d8561fed31e054e073574451da988aa6bea375
romaindegivry <romain.degivry@hotmail.fr>
2017-08-07 03:01:44 -0700

    added instalation script

---
+++ python/python-setups.sh
@@ -0,0 +1,19 @@
+#This script will hopefully install the dependancies for the python files
+#this works on ubuntu 16 LTS
+
+#to run this open a shell in this file's directory and use the command
+#bash python-setups.sh
+
+sudo apt-get install python pip
+
+sudo pip install --upgrade pip
+
+#now get the packages
+sudo pip install numpy matplotlib
+
+#do some cool end sript message
+echo '--------------------------------------------'
+echo '--------------- Script ended ---------------'
+echo '--------------------------------------------'
+
+
