256625da2e0eb37105f764731c76069f4c39b706
AlexMontgomerie <am9215@ic.ac.uk>
2017-10-29 21:39:03 +0000

    added outline for metric generator

---
+++ python/src/metric_gen.py
@@ -0,0 +1,13 @@
+'''
+This source file will take all the tables as input.
+Need to get metrics for
+  - peak/average gain
+  - size of antenna (length, radius)
+  - front-to-back ratio
+  - fit to ideal radiation pattern
+  - efficiency
+  - power of output
+  - directivity
+'''
+
+
